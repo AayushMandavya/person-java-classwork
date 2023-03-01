aabfaeae168f3d972e63cee821f8730bd48cb9dd
aayushmandavya <mandavyaaayush98@gmail.com>
2023-03-01 10:16:52 +0545

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        person object=new person(10,"Aayush") ;
+//        System.out.println(object.width);
+//        System.out.println(object.length);
+        object.introduce();
+
+    }
+}
---
+++ src/person.java
@@ -0,0 +1,12 @@
+public class person {
+int age;
+String name;
+person(int a,String n){
+age=a;
+name=n;
+
+}
+    public void introduce(){
+        System.out.println("Age is "+age+"Name is:"+name);
+    }
+}
